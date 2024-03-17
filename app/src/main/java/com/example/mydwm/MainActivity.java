328fec418ffee952df3bd5b76464cba2e71b1390
Mikhail Sorokin <mikleso@mail.ru>
2024-03-17 16:18:36 +0300

    начало

---
+++ app/src/main/java/com/example/mydwm/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.mydwm;
+
+import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+
+public class MainActivity extends Activity {
+    private Button button;
+    private ImageView imageView;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        imageView = findViewById(R.id.imageView);
+        //button = findViewById(R.id.imageButton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, 1);
+            }
+        });
+        /*File imageFile = new File(Environment.getExternalStorageDirectory() + "/your_image_name.png");
+        if (imageFile.exists()) {
+            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+            BitmapDrawable drawable = new BitmapDrawable(getResources(), bitmap);
+            Drawable drawable = getResources().getDrawable(R.drawable.imageview);
+            ImageView imageView = (ImageView) findViewById(R.id.imageView);
+            imageView.setImageDrawable(drawable);
+        }*/
+    }
+
+    //@Override
+    protected void onActivityResult(Integer requestCode, Integer resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = {MediaStore.Images.Media.DATA};
+            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+            cursor.moveToFirst();
+            Integer columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+        }
+    }
+}

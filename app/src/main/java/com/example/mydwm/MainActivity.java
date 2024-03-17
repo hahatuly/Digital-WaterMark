1b14993050777b540d2ffb0a9894f92c18d7b2be
Mikhail Sorokin <mikleso@mail.ru>
2024-03-17 21:12:14 +0300

    работает открытие фотки!

--- app/src/main/java/com/example/mydwm/MainActivity.java
+++ app/src/main/java/com/example/mydwm/MainActivity.java
@@ -3,20 +3,15 @@ package com.example.mydwm;
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.graphics.drawable.BitmapDrawable;
-import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
-import java.io.File;
+import com.bumptech.glide.Glide;
+
 
 public class MainActivity extends Activity {
     private Button button;
@@ -30,7 +25,7 @@ public class MainActivity extends Activity {
 
         button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
-        //button = findViewById(R.id.imageButton);
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,28 +34,14 @@ public class MainActivity extends Activity {
                 startActivityForResult(intent, 1);
             }
         });
-        /*File imageFile = new File(Environment.getExternalStorageDirectory() + "/your_image_name.png");
-        if (imageFile.exists()) {
-            Bitmap bitmap = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
-            BitmapDrawable drawable = new BitmapDrawable(getResources(), bitmap);
-            Drawable drawable = getResources().getDrawable(R.drawable.imageview);
-            ImageView imageView = (ImageView) findViewById(R.id.imageView);
-            imageView.setImageDrawable(drawable);
-        }*/
+
     }
 
-    //@Override
-    protected void onActivityResult(Integer requestCode, Integer resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
             Uri selectedImage = data.getData();
-            String[] filePathColumn = {MediaStore.Images.Media.DATA};
-            Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
-            cursor.moveToFirst();
-            Integer columnIndex = cursor.getColumnIndex(filePathColumn[0]);
-            String picturePath = cursor.getString(columnIndex);
-            cursor.close();
-            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            Glide.with(this).load(selectedImage).into(imageView);
         }
     }
 }

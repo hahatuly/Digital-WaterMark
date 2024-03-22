7002ccb2775c7f3c7ba556821ac5669c19f0c15c
Mikhail Sorokin <mikleso@mail.ru>
2024-03-22 12:55:17 +0300

    гг

--- app/src/main/java/com/example/mydwm/MainActivity.java
+++ app/src/main/java/com/example/mydwm/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.mydwm;
 
-
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -10,15 +9,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
-
 import com.bumptech.glide.Glide;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
@@ -67,12 +63,9 @@ public class MainActivity extends Activity {
         Intent intent = new Intent(android.content.Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         intent.setData(outputFileUri);
         sendBroadcast(intent);
-
         try
         {
-            //Uri uri = data.getData();
             FileOutputStream fos = new FileOutputStream(outputFileUri.getPath());
-            //Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
             Toast.makeText(this, "Image saved successfully", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
@@ -84,8 +77,7 @@ public class MainActivity extends Activity {
     private Uri getOutputMediaFileUri() {
         String timeStamp = new java.text.SimpleDateFormat("yyyyMMdd_HHmmss").format(new java.util.Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "DigitalWatermarksImages");
         if (!mediaStorageDir.exists()) {
             if (!mediaStorageDir.mkdirs()) {
                 return null;

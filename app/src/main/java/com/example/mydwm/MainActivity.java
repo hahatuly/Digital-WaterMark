ac436f405c7d9cb0b963f6c1e3dec5c7a88f0705
Mikhail Sorokin <mikleso@mail.ru>
2024-03-22 12:40:42 +0300

    заработало

--- app/src/main/java/com/example/mydwm/MainActivity.java
+++ app/src/main/java/com/example/mydwm/MainActivity.java
@@ -10,156 +10,70 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 
-public class MainActivity extends Activity
-{
-    private static final int REQUEST_CODE_PICK_IMAGE = 101;
-    private static final int REQUEST_CODE_SAVE_IMAGE = 102;
+public class MainActivity extends Activity {
     private ImageView imageView;
+    private Button imageButton;
     private Button saveButton;
-    private Bitmap bitmap;
-    private File currentImageFile;
-
-    private static final int SELECT_PICTURE = 0;
-    private Button button;
-    //private ImageView imageView;
-    //private Button saveButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
+        imageButton = findViewById(R.id.imageButton);
         imageView = findViewById(R.id.imageView);
         saveButton = findViewById(R.id.saveButton);
 
-        button.setOnClickListener(new View.OnClickListener()
-        {
+        imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
                 startActivityForResult(intent, 1);
             }
         });
-
         saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-                startActivityForResult(intent, 2);
                 Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-                try {
-                    saveImage(bitmap);
-                } catch (FileNotFoundException e) {
-                    throw new RuntimeException(e);
-                }
+                saveImage(bitmap);
             }
         });
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data)
-    {
-
-        if (requestCode == 1 && resultCode == RESULT_OK && data != null)
-        {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
             Uri selectedImage = data.getData();
             Glide.with(this).load(selectedImage).into(imageView);
         }
-
     }
 
-    private void saveImage(Bitmap bitmap) throws FileNotFoundException {
-        String timeStamp = new java.text.SimpleDateFormat("yyyyMMdd_HHmmss").format(new java.util.Date());
-        String imageFileName = "JPEG_" + timeStamp + "_";
-        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
-                Environment.DIRECTORY_PICTURES), "MyCameraApp");
-
-        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + imageFileName + ".jpg");
-        Uri outputFileUri = Uri.fromFile(mediaFile);
-
+    private void saveImage(Bitmap bitmap) {
+        Uri outputFileUri = getOutputMediaFileUri();
         Intent intent = new Intent(android.content.Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         intent.setData(outputFileUri);
         sendBroadcast(intent);
 
-        FileOutputStream fos = null;
-        //String path = "/myImage.jpg";
-        fos = new FileOutputStream(mediaFile);
-        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-    }
-        /*button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-                startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
-            }
-        });*/
-        /*imageView.setOnClickListener(new View.OnClickListener()
+        try
         {
-            @Override
-            public void onClick(View v)
-            {
-                openImagePicker();
-            }
-        });
-        saveButton.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View v)
-            {
-                saveImage();
-            }
-        });
-    }
-    private void openImagePicker() {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-        {
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_PICK_IMAGE);
-        }
-        else
-        {
-            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-            startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
-        }
-    }*/
-
-    /*private void saveImage() {
-        if (bitmap != null) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                    != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                        REQUEST_CODE_SAVE_IMAGE);
-            } else {
-                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/jpeg");
-                intent.putExtra(Intent.EXTRA_TITLE, "new_image.jpg");
-                startActivityForResult(intent, REQUEST_CODE_SAVE_IMAGE);
-            }
-        } else {
-            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
-        }
-    }
-    private void saveImageToUri(Uri uri) {
-        try {
-            FileOutputStream fos = new FileOutputStream(uri.getPath());
+            //Uri uri = data.getData();
+            FileOutputStream fos = new FileOutputStream(outputFileUri.getPath());
+            //Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
             bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-            fos.flush();
-            fos.close();
             Toast.makeText(this, "Image saved successfully", Toast.LENGTH_SHORT).show();
         } catch (IOException e) {
             e.printStackTrace();
@@ -167,25 +81,18 @@ public class MainActivity extends Activity
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        if (resultCode == Activity.RESULT_OK) {
-            if (requestCode == REQUEST_CODE_PICK_IMAGE) {
-                Uri selectedImageUri = data.getData();
-                String imagePath = selectedImageUri.getPath();
-                currentImageFile = new File(imagePath);
-                bitmap = BitmapFactory.decodeFile(imagePath);
-
-                //imageView.setImageBitmap(bitmap);
-                Glide.with(this).load(selectedImageUri).into(imageView);
-            } else if (requestCode == REQUEST_CODE_SAVE_IMAGE) {
-                if (data != null && data.getData() != null) {
-                    Uri uri = data.getData();
-                    saveImageToUri(uri);
-                }
+    private Uri getOutputMediaFileUri() {
+        String timeStamp = new java.text.SimpleDateFormat("yyyyMMdd_HHmmss").format(new java.util.Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), "MyCameraApp");
+        if (!mediaStorageDir.exists()) {
+            if (!mediaStorageDir.mkdirs()) {
+                return null;
             }
         }
-    }*/
+        File mediaFile = new File(mediaStorageDir.getPath() + File.separator + imageFileName + ".jpg");
+        Uri outputFileUri = Uri.fromFile(mediaFile);
+        return outputFileUri;
+    }
 }

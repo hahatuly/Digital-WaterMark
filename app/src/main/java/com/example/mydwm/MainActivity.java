12cfe80d0bef0e62453bc4574052894b14eeb225
Mikhail Sorokin <mikleso@mail.ru>
2024-03-22 07:42:55 +0300

    каша мала

--- app/src/main/java/com/example/mydwm/MainActivity.java
+++ app/src/main/java/com/example/mydwm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mydwm;
 
+
 import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
@@ -16,14 +17,22 @@ import android.widget.ImageView;
 import com.bumptech.glide.Glide;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
+public class MainActivity extends Activity
+{
+    private static final int REQUEST_CODE_PICK_IMAGE = 101;
+    private static final int REQUEST_CODE_SAVE_IMAGE = 102;
+    private ImageView imageView;
+    private Button saveButton;
+    private Bitmap bitmap;
+    private File currentImageFile;
 
-
-public class MainActivity extends Activity {
     private static final int SELECT_PICTURE = 0;
     private Button button;
-    private ImageView imageView;
-    private Button save_button;
+    //private ImageView imageView;
+    //private Button saveButton;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -34,7 +43,7 @@ public class MainActivity extends Activity {
 
         button = findViewById(R.id.button);
         imageView = findViewById(R.id.imageView);
-        save_button = findViewById(R.id.save_button);
+        saveButton = findViewById(R.id.saveButton);
 
         button.setOnClickListener(new View.OnClickListener()
         {
@@ -46,13 +55,17 @@ public class MainActivity extends Activity {
             }
         });
 
-        save_button.setOnClickListener(new View.OnClickListener() {
+        saveButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+                startActivityForResult(intent, 2);
                 Bitmap bitmap = ((BitmapDrawable) imageView.getDrawable()).getBitmap();
-                saveImage(bitmap);
-                //Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
-                //startActivityForResult(intent, 2);
+                try {
+                    saveImage(bitmap);
+                } catch (FileNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
             }
         });
     }
@@ -69,29 +82,110 @@ public class MainActivity extends Activity {
 
     }
 
-    private void saveImage(Bitmap bitmap)
-    {
-        Uri outputFileUri = getOutputMediaFileUri();
-        Intent intent = new Intent(android.content.Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-        intent.setData(outputFileUri);
-        sendBroadcast(intent);
-    }
-
-    private Uri getOutputMediaFileUri()
-    {
+    private void saveImage(Bitmap bitmap) throws FileNotFoundException {
         String timeStamp = new java.text.SimpleDateFormat("yyyyMMdd_HHmmss").format(new java.util.Date());
         String imageFileName = "JPEG_" + timeStamp + "_";
         File mediaStorageDir = new File(Environment.getExternalStoragePublicDirectory(
                 Environment.DIRECTORY_PICTURES), "MyCameraApp");
 
-        if (!mediaStorageDir.exists()) {
-            if (!mediaStorageDir.mkdirs()) {
-                return null;
-            }
-        }
-
         File mediaFile = new File(mediaStorageDir.getPath() + File.separator + imageFileName + ".jpg");
         Uri outputFileUri = Uri.fromFile(mediaFile);
-        return outputFileUri;
+
+        Intent intent = new Intent(android.content.Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(outputFileUri);
+        sendBroadcast(intent);
+
+        FileOutputStream fos = null;
+        //String path = "/myImage.jpg";
+        fos = new FileOutputStream(mediaFile);
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
     }
+        /*button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+            }
+        });*/
+        /*imageView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                openImagePicker();
+            }
+        });
+        saveButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                saveImage();
+            }
+        });
+    }
+    private void openImagePicker() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+        {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_CODE_PICK_IMAGE);
+        }
+        else
+        {
+            Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, REQUEST_CODE_PICK_IMAGE);
+        }
+    }*/
+
+    /*private void saveImage() {
+        if (bitmap != null) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                    != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_SAVE_IMAGE);
+            } else {
+                Intent intent = new Intent(Intent.ACTION_CREATE_DOCUMENT);
+                intent.addCategory(Intent.CATEGORY_OPENABLE);
+                intent.setType("image/jpeg");
+                intent.putExtra(Intent.EXTRA_TITLE, "new_image.jpg");
+                startActivityForResult(intent, REQUEST_CODE_SAVE_IMAGE);
+            }
+        } else {
+            Toast.makeText(this, "No image selected", Toast.LENGTH_SHORT).show();
+        }
+    }
+    private void saveImageToUri(Uri uri) {
+        try {
+            FileOutputStream fos = new FileOutputStream(uri.getPath());
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+            fos.flush();
+            fos.close();
+            Toast.makeText(this, "Image saved successfully", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Failed to save image", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == REQUEST_CODE_PICK_IMAGE) {
+                Uri selectedImageUri = data.getData();
+                String imagePath = selectedImageUri.getPath();
+                currentImageFile = new File(imagePath);
+                bitmap = BitmapFactory.decodeFile(imagePath);
+
+                //imageView.setImageBitmap(bitmap);
+                Glide.with(this).load(selectedImageUri).into(imageView);
+            } else if (requestCode == REQUEST_CODE_SAVE_IMAGE) {
+                if (data != null && data.getData() != null) {
+                    Uri uri = data.getData();
+                    saveImageToUri(uri);
+                }
+            }
+        }
+    }*/
 }
